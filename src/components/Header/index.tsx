6c993c91b0051549533422067093de9b2e99a284
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-02-23 14:30:43 -0300

    header basico

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,22 @@
-export function Header(){
+import { MapPin, ShoppingCart } from "@phosphor-icons/react";
+import { CartIcon, HeaderContainer, NavContainer } from "./styles";
+import logo from "../../assets/Logo.svg";
+
+export function Header() {
   return (
-    <header>
-      <h1>Teste Header</h1>
-    </header>
-  )
+    <HeaderContainer>
+      <div>
+        <img src={logo} alt="" />
+      </div>
+      <NavContainer>
+        <div>
+          <MapPin size={32} />
+          <p>Porto Alagre, RS</p>
+        </div>
+        <CartIcon>
+          <ShoppingCart size={32} color="#C47F17"/>
+        </CartIcon>
+      </NavContainer>
+    </HeaderContainer>
+  );
 }
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,1 +1,26 @@
 import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 0px;
+`;
+
+export const NavContainer = styled.header`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const CartIcon = styled.div`
+  width: 32px;
+  height: 32px;
+  padding: 4px;
+  background-color: #F1E9C9;
+  border-radius: 4px;
+`
